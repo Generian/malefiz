158aaf7064b7de50aa3e15018ba8760c6097b7e6
Sebastian Günther <sguenther3141@gmail.com>
2023-08-15 23:50:27 +0200

    annotate

--- utils/analytics.ts
+++ utils/analytics.ts
@@ -1,6 +1,7 @@
 import ReactGA from 'react-ga'
 
 export const initGoogleAnalytics = () => {
+  // Import measurement ID
   const NEXT_PUBLIC_GA_TRACKING_ID: string = process.env.NEXT_PUBLIC_GA_TRACKING_ID as string
 
   ReactGA.initialize(NEXT_PUBLIC_GA_TRACKING_ID)
