8706de7e17ac0abd34d975400c372aed4c34fd63
Sebastian Günther <sguenther3141@gmail.com>
2023-08-16 00:26:27 +0200

    add custom script back

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,6 +5,7 @@ import ReactGA from 'react-ga'
 import { initGoogleAnalytics } from 'src/utils/analytics'
 import { useRouter } from 'next/router'
 import { useEffect } from 'react'
+import Script from 'next/script'
 
 export default function App({ Component, pageProps }: AppProps) {
   const router = useRouter()
@@ -29,6 +30,19 @@ export default function App({ Component, pageProps }: AppProps) {
 
   return <>
     <link href='https://fonts.googleapis.com/css?family=Arbutus' rel='stylesheet'></link>
+    <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-VX66E5NXM6"
+        strategy="afterInteractive"
+      />
+    <Script id="google-analytics" strategy="afterInteractive">
+      {`
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'G-VX66E5NXM6');
+      `}
+    </Script>
     <Component {...pageProps} />
   </>
 }
