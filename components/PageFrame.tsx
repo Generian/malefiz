6a62c4bcfa2c8f0967d0c0a426682153946348f8
Sebastian Günther <sguenther3141@gmail.com>
2023-08-20 12:47:39 +0200

    try enzuzo for cookie management

--- components/PageFrame.tsx
+++ components/PageFrame.tsx
@@ -15,7 +15,8 @@ export default function PageFrame({ children, title, noZoom = false }: PageFrame
         {!noZoom && <meta name="viewport" content="width=device-width, initial-scale=1" />}
         {noZoom && <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0" />}
         <link rel="icon" href="/favicon.ico" />
-        <script id="Cookiebot" src="https://consent.cookiebot.com/uc.js" data-cbid="8bfe88c6-fdaf-41f1-a309-756278ef2be8" data-blockingmode="auto" type="text/javascript"></script>
+        {/* <script id="Cookiebot" src="https://consent.cookiebot.com/uc.js" data-cbid="8bfe88c6-fdaf-41f1-a309-756278ef2be8" data-blockingmode="auto" type="text/javascript"></script> */}
+        <script src="https://app.enzuzo.com/apps/enzuzo/static/js/__enzuzo-cookiebar.js?uuid=cdd6de02-3f46-11ee-8b2a-57763a880aa1"></script>
       </Head>
       <main>
         {children}
