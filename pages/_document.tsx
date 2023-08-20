7a75e6288770dbb8ed9a12b53e9c669de1874bfd
Sebastian Günther <sguenther3141@gmail.com>
2023-08-20 13:07:48 +0200

    try cookieyes

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -7,7 +7,8 @@ export default function Document() {
     <Html lang="en">
       <Head>
         <meta name="google-site-verification" content="wUHmAjX7bgZFcGc6KDRvZ6NUVWi38HBSbDgfsEBIkH8" />
-        <script src="https://app.enzuzo.com/apps/enzuzo/static/js/__enzuzo-cookiebar.js?uuid=cdd6de02-3f46-11ee-8b2a-57763a880aa1"></script>
+        {/* <script src="https://app.enzuzo.com/apps/enzuzo/static/js/__enzuzo-cookiebar.js?uuid=cdd6de02-3f46-11ee-8b2a-57763a880aa1"></script> */}
+        <script id="cookieyes" type="text/javascript" src="https://cdn-cookieyes.com/client_data/0878e4eb4e8cdda52cb078fc/script.js"></script>
       </Head>
       <body>
         <ThemeProvider theme={theme}>
